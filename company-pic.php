e321c5ea0587b211d09f3cb2b99144fe04e35394
Panathani <99027834+Panathani@users.noreply.github.com>
2022-05-28 22:05:59 +0700

    Update company-pic.php

--- company-pic.php
+++ company-pic.php
@@ -13,7 +13,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>หน้าบริษัท · WeWork</title>
+    <title>ภาพบริษัท · WeWork</title>
     <link href="css/main.css" rel="stylesheet" type="text/css" />
     <link href="css/style.css" rel="stylesheet" type="text/css" />
     <!-- <link href="css/style2.css" rel="stylesheet" type="text/css" /> -->
