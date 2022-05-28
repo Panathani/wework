0ee06decd0a0f884406320ca7fbc83325dbce11e
Panathani <99027834+Panathani@users.noreply.github.com>
2022-05-28 21:59:14 +0700

    Update company-applicant.php

--- company-applicant.php
+++ company-applicant.php
@@ -12,7 +12,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ตำแหน่งงานทั้งหมด · WeWork</title>
+    <title>ผู้สมัครทั้งหมด · WeWork</title>
     <link href="css/style.css" rel="stylesheet" type="text/css" />
     <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
     <script src="js/script.js"></script>
