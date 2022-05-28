bef165cf4cc4d0ff879bc0a36a05f748275929d7
Panathani <99027834+Panathani@users.noreply.github.com>
2022-05-28 22:07:26 +0700

    Update index-company.php

--- index-company.php
+++ index-company.php
@@ -10,7 +10,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>หน้าหลัก · WeWork</title>
+    <title>Index Company · WeWork</title>
     <link href="css/style.css" rel="stylesheet" type="text/css">
     <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
